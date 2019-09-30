1eea07a581b72e5cfa1e5574910e9b9094afac7e
Eugene Bekker <ebekker@users.noreply.github.com>
2019-09-30 14:21:54 -0400

    checking in firt CI workflow

---
+++ src/Bkkr.GitHub.TestActionsAndRegistry/Simple1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Bkkr.GitHub.TestActionsAndRegistry
+{
+    public static class Simple1
+    {
+        public static string HelloWorld(string who = "World") => $"Hello {who}!";
+    }
+}
