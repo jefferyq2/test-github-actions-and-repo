29adb5fad26ffa5a41f0ee85a8497d51f4a3043f
Eugene Bekker <ebekker@users.noreply.github.com>
2019-10-02 17:05:32 -0400

    test

--- .github/actions/build-num/dist/index.js
+++ .github/actions/build-num/dist/index.js
@@ -327,7 +327,13 @@ async function run() {
     const gist_token = core.getInput('gist_token');
     const version_key = core.getInput('version_key');
 
-    await exec.exec('pwsh', ['-f', 'build-num.ps1']);
+    await exec.exec('pwsh', [
+      '-f', './.github/actions/build-num/build-num.ps1',
+      '-InformationAction', 'Continue',
+       '-GitHubToken', gist_token,
+       '-VersionKey', version_key,
+    ]);
+
 
   /*
     const octokit = new github.GitHub(github_token);
