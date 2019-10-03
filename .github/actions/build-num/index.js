35982a9a05c0736344ce46d5a79e9168f22e4344
Eugene Bekker <ebekker@users.noreply.github.com>
2019-10-03 09:36:35 -0400

    test

--- .github/actions/build-num/index.js
+++ .github/actions/build-num/index.js
@@ -1,37 +1,21 @@
 
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-//const github = require('@actions/github');
 
 async function run() {
-  try {
-    const gist_token = core.getInput('gist_token');
-    const version_key = core.getInput('version_key');
+    try {
+        const gist_token = core.getInput('gist_token');
+        const version_key = core.getInput('version_key');
 
-    await exec.exec('pwsh', [
-      '-f', './.github/actions/build-num/build-num.ps1',
-      '-InformationAction', 'Continue',
-       '-GitHubToken', gist_token,
-       '-VersionKey', version_key,
-    ]);
-
-
-  /*
-    const octokit = new github.GitHub(github_token);
-
-    core.setOutput("build_num", "99");
-    core.exportVariable('BUILD_NUM', '99');
-
-    /*
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-    */
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        await exec.exec('pwsh', [
+            '-f', './.github/actions/build-num/build-num.ps1',
+            '-InformationAction', 'Continue',
+            '-GitHubToken', gist_token,
+            '-VersionKey', version_key,
+        ]);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
-  
+
 run();
