093431c9e23801fa0afc5d362a9b7c1b6b39bd65
Eugene Bekker <ebekker@users.noreply.github.com>
2019-10-03 09:49:23 -0400

    test

--- .github/actions/invoke-pwsh/dist/index.js
+++ .github/actions/invoke-pwsh/dist/index.js
@@ -318,7 +318,12 @@ const exec = __webpack_require__(806);
 
 async function run() {
     try {
-        await exec.exec('pwsh', [ '-c', 'Write-Host $pwd' ]);
+        await exec.exec('pwsh', [ '-c', '"Write-Host \'***** Current Directory:\'"' ]);
+        await exec.exec('pwsh', [ '-c', '"Write-Host $pwd"' ]);
+        await exec.exec('pwsh', [ '-c', '"Write-Host \'***** List Directory:\'"' ]);
+        await exec.exec('pwsh', [ '-c', '"Get-ChildItem ."' ]);
+        await exec.exec('pwsh', [ '-c', '"Write-Host \'***** List Environment:\'"' ]);
+        await exec.exec('pwsh', [ '-c', '"Get-ChildItem env:"' ]);
     } catch (error) {
         core.setFailed(error.message);
     }
