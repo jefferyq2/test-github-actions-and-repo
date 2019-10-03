c9002e069220db4e0e0102cc251ee2a392072a84
Eugene Bekker <ebekker@users.noreply.github.com>
2019-10-03 09:54:57 -0400

    test

--- .github/actions/invoke-pwsh/index.js
+++ .github/actions/invoke-pwsh/index.js
@@ -5,11 +5,11 @@ const exec = require('@actions/exec');
 async function run() {
     try {
         await exec.exec('pwsh', [ '-c', '"Write-Host \'***** Current Directory:\'"' ]);
-        await exec.exec('pwsh', [ '-c', '"Write-Host $pwd"' ]);
+        await exec.exec('pwsh', [ '-c', '"Write-Host $pwd | Out-Default | Write-Host"' ]);
         await exec.exec('pwsh', [ '-c', '"Write-Host \'***** List Directory:\'"' ]);
-        await exec.exec('pwsh', [ '-c', '"Get-ChildItem ."' ]);
+        await exec.exec('pwsh', [ '-c', '"Get-ChildItem . | Out-Default | Write-Host"' ]);
         await exec.exec('pwsh', [ '-c', '"Write-Host \'***** List Environment:\'"' ]);
-        await exec.exec('pwsh', [ '-c', '"Get-ChildItem env:"' ]);
+        await exec.exec('pwsh', [ '-c', '"Get-ChildItem env: | Out-Default | Write-Host"' ]);
     } catch (error) {
         core.setFailed(error.message);
     }
